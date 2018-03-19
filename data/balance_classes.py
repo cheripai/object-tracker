cd9ab0a53058b4e04bb45e533c5c65eff1d3e5f5
dat <datdo@rocketmail.com>
2018-03-19 16:41:21 -0700

    Add balance_classes.py

---
+++ data/balance_classes.py
@@ -0,0 +1,51 @@
+import os
+import sys
+from random import shuffle
+
+def get_category_file_dict(csv_path):
+    category_file = {}
+    with open(csv_path) as f:
+        header = f.readline().strip()
+        for line in f.readlines():
+            line = line.strip()
+            fname, width, height, category, xmin, ymin, xmax, ymax = line.split(",")
+            file_id = os.path.basename(fname)
+            if category in category_file:
+                if file_id in category_file[category]:
+                    category_file[category][file_id].append(line)
+                else:
+                    category_file[category][file_id] = [line]
+            else:
+                category_file[category] = {file_id:[line]}
+    return category_file, header
+
+if __name__ == "__main__":
+    category_file, header = get_category_file_dict(sys.argv[1])
+
+    min_images = min([len(category_file[category]) for category in category_file.keys()])
+    min_images = int(min_images)
+
+    for category in category_file.keys():
+        if len(category_file[category]) > min_images:
+            fnames = list(category_file[category].keys())
+            shuffle(fnames)
+            for i in range(len(fnames) - min_images):
+                for category in category_file.keys():
+                    if fnames[i] in category_file[category]:
+                        del category_file[category][fnames[i]]
+
+    annotations = []
+    for category in category_file.keys():
+        num_annotations = 0
+        for file_id in category_file[category]:
+            for annotation in category_file[category][file_id]:
+                num_annotations += 1
+                annotations.append(annotation)
+        print(category, len(category_file[category]), num_annotations)
+
+    shuffle(annotations)
+    annotations.insert(0, header)
+    
+    with open(sys.argv[2], "w+") as f:
+        for annotation in annotations:
+            f.write(annotation + "\n")
